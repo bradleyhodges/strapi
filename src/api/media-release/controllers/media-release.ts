c3444f818ed0bd8aeec42c15d0f2937531c1a4c9
Bradley Hodges <bradley.hodges@outlook.com>
2024-10-26 18:17:19 +0800

    Create content schema

---
+++ src/api/media-release/controllers/media-release.ts
@@ -0,0 +1,7 @@
+/**
+ * media-release controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::media-release.media-release');
---
+++ src/api/media-release/routes/media-release.ts
@@ -0,0 +1,7 @@
+/**
+ * media-release router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::media-release.media-release');
